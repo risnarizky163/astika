26c48070f53a8edbff5ba68ceb9a1211b8e0f3c8
astika <astikasari.sdm@gmail.com>
2025-07-03 15:51:53 +0700

    first land

---
+++ sekolah-app-main/views/home/index.php
@@ -0,0 +1,14 @@
+<div class="contianer-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <h1>SMK <span class="text-primary">Negeri</span> Semarang</h1>
+            <p>adalah sekolah yang mencetak penerus bangsa yang berkualitas</p>
+            <button type="button" class="btn btn-primary position-relative"> Daftar Tahun Depan </button>
+            </div>
+        <div class="col">
+        <img src="https://www.smpn15yk.sch.id/wp-content/uploads/2022/03/LORONG-KELAS-LANTAI-1.jpg" class="rounded img-thumbnail" width="500"/>
+        </div>
+        </div>
+    </div>
+</div>
